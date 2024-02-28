c09dc31254926e15c966131f4a320e9648068e60
Iohel <joelpons9@hotmail.com>
2024-02-28 21:14:27 +0100

    repository

--- js/memory.js
+++ js/memory.js
@@ -1,13 +1,13 @@
-
-function memoryGame() {
-    let seconds = 0;
+let seconds = 0;
     let colors = ["blue","red","purple","green","orange","yellow","silver","pink","brown","aqua"];
     let score = 99999;
     let clues = 0;
     let finalColor = [];
     let crono = null;
-    let pairs = 25;
+    let pairs = 32;
     let array = [];
+function memoryGame() {
+    
     for (let i = 1; i <= pairs; i++) {
             
         array.push(i);
@@ -29,148 +29,98 @@ function memoryGame() {
 
     let value1 = -1;
     let value2 = -1;
+    let block = false;
     let cleared = 0;
     document.querySelectorAll("div").forEach(element => {
         element.addEventListener("click",function(e){
             if(seconds === 0){
                 crono  = setInterval(counter,1000);
             }
-            if(value1 !== -1){
-                value2 = e.target.innerText;
-                if(value1 == value2){
-                    
-                    e.target.className = "revealed";
-                    let random = Math.ceil((Math.random())*colors.length);
-                    console.log(random-1);
-                    let color = colors[random-1];
-                    
-                    setTimeout(()=>{
-                        document.querySelectorAll(".revealed").forEach(element => {
-                            element.setAttribute('class','cleared '+color);
-                        });
-                    },500)
-                    
-                    value1 = -1;
-                    value2 = -1;
-                    cleared++;
-
-                    if (pairs==cleared) {
+            if(!block){
+                if(value1 !== -1){
+                    value2 = e.target.innerText;
+                    block = true;
+                    if(value1 == value2){
                         
-                        let code = [];
-                        setTimeout(() => {
-                            
-                            colors.forEach(color => {
-                                let win = new Set();
-                                let colors = new Set();
-                                if(document.querySelectorAll("."+color).length/2 >= 4){
-                                    document.querySelectorAll("."+color).forEach(number =>{
-                                        win.add(number.innerText);
-                                        
-                                    })
-                                    code.push(win);
-                                    colors.add(color);
-                                }
-
+                        e.target.className = "revealed";
+                        let random = Math.ceil((Math.random())*colors.length);
+                        console.log(random-1);
+                        let color = colors[random-1];
+                       
+                        setTimeout(()=>{
+                            document.querySelectorAll(".revealed").forEach(element => {
+                                element.setAttribute('class','cleared '+color);
                             });
-                            let t = Math.floor(Math.random())*code.length;
-                            console.log(code);
-                            if(code.length != 1){
-                                code = [...code[t]];
-                                finalColor = colors[t];
-                            }else{
-                                code = [...code[0]];
-                                finalColor = colors[0];
-                            }
-                            document.querySelector(".contenidor").style.display = 'none';
-                            document.querySelector(".envoltorio-popup").style.display = 'block';
-                            document.querySelector(".envoltorio-popup").classList.add(finalColor);
-
-                            for (let i = 0; i < 4; i++) {
-                                console.log(code[i]);
-                                let inputs = document.querySelectorAll(".input");
-                                inputs[i].id = code[i];
-                            }
+                            block = false;
+                        },500)
+                        
+                        value1 = -1;
+                        value2 = -1;
+                        cleared++;
+    
+                        if (pairs==cleared) {
                             
-                            
-                        }, 3000);
+                            let code = [];
+                            setTimeout(() => {
+                                
+                                colors.forEach(color => {
+                                    let win = new Set();
+                                    let colors = new Set();
+                                    if(document.querySelectorAll("."+color).length/2 >= 4){
+                                        document.querySelectorAll("."+color).forEach(number =>{
+                                            win.add(number.innerText);
+                                            
+                                        })
+                                        code.push(win);
+                                        colors.add(color);
+                                    }
+    
+                                });
+                                let t = Math.floor(Math.random())*code.length;
+                                console.log(code);
+                                if(code.length != 1){
+                                    code = [...code[t]];
+                                    finalColor = colors[t];
+                                }else{
+                                    code = [...code[0]];
+                                    finalColor = colors[0];
+                                }
+                                document.querySelector(".contenidor").style.display = 'none';
+                                document.querySelector(".envoltorio-popup").style.display = 'block';
+                                document.querySelector(".popup").classList.add(finalColor);
+    
+                                for (let i = 0; i < 4; i++) {
+                                    console.log(code[i]);
+                                    let inputs = document.querySelectorAll(".input");
+                                    inputs[i].id = code[i];
+                                }
+                                
+                                
+                            }, 3000);
+                        }
+                    }else{
+                        e.target.className = "revealed";
+                        
+                        setTimeout(()=>{
+                            document.querySelectorAll(".revealed").forEach(element => {
+                                element.removeAttribute('class')
+                            });
+                            block = false;
+                        },1000);
+                        
+                        value1 = -1;
+                        value2 = -1;
+        
                     }
                 }else{
+                    value1 = e.target.innerText;
                     e.target.className = "revealed";
                     
-                    setTimeout(()=>{
-                        document.querySelectorAll(".revealed").forEach(element => {
-                            element.removeAttribute('class')
-                        });
-                    },1000);
-                    
-                    value1 = -1;
-                    value2 = -1;
-    
                 }
-            }else{
-                value1 = e.target.innerText;
-                e.target.className = "revealed";
-                
             }
+            
         })
     });
-    
-    document.querySelector(".solve").addEventListener("click",function(){
-        let correct = true;
-        document.querySelectorAll(".input").forEach(element => {
-            if(element.id !== element.value){
-                correct = false;
-            }else{
-                console.log(":D");
-            }
-        });
-
-        if(correct){
-            clearInterval(crono);
-            score = score-(seconds*100)-(clues*10000);
-            document.querySelector(".contenido-popup").innerHTML=
-            "<section>"+
-                "<h1>Final Score</h1>"+
-                "<h6>"+score+"</h6>"+
-                "<a href='./menu.html'>To the menu.</a>"
-            "</section>";
-            let record = localStorage.getItem("Memory");
-            record = JSON.parse(record);
-
-            if(record != null){
-                
-                record.push([item.username,score]);
-                localStorage.setItem("Memory",JSON.stringify(record));
-            }else{
-                finalScore = [[item.username,score]];
-                localStorage.setItem("Memory",JSON.stringify(finalScore));
-            }
-        }
-    })
-
-    document.querySelector(".clue").addEventListener("click",function (){
-        let output = document.querySelector(".clue_output");
-        clues++
-        console.log(clues);
-        switch (clues) {
-            case 1:
-                output.innerHTML = output.innerHTML + "<p>Think of the colors.</p>";
-                break;
-        
-            case 2:
-                output.innerHTML = output.innerHTML + "<p>The order matters.</p>";
-                break;
-
-            case 3:
-                output.innerHTML = output.innerHTML + "<p>The first 4 numbers.</p>";                
-                break;
-        
-            default:
-                clues = 3;
-                break;
-        }
-    })
-    
 
     function counter(){
         seconds += 1;
@@ -178,3 +128,52 @@ function memoryGame() {
 }
 
 memoryGame();
+
+document.querySelector(".solve").addEventListener("click",function(){
+    let correct = true;
+    document.querySelectorAll(".input").forEach(element => {
+        if(element.id !== element.value){
+            correct = false;
+        }else{
+            console.log(":D");
+        }
+    });
+
+    if(correct){
+        clearInterval(crono);
+        score = score-(seconds*100)-(clues*10000);
+        document.querySelector(".contenido-popup").innerHTML=
+        "<section>"+
+            "<h1>Final Score</h1>"+
+            "<h6>"+score+"</h6>"+
+            "<a href='./menu.html'>To the menu.</a>"
+        "</section>";
+        let record = localStorage.getItem("Memory");
+        record = JSON.parse(record);
+
+        if(record != null){
+            
+            record.push([item.username,score]);
+            localStorage.setItem("Memory",JSON.stringify(record));
+        }else{
+            finalScore = [[item.username,score]];
+            localStorage.setItem("Memory",JSON.stringify(finalScore));
+        }
+    }
+})
+
+document.querySelector(".clue").addEventListener("click",function (){
+    let output = document.querySelector(".clue_output");
+    clues++;
+    
+    if (clues === 1) {
+        output.innerHTML = output.innerHTML + "<p>Think of the colors.</p>";
+    }else if (clues === 2) {
+        output.innerHTML = output.innerHTML + "<p>The order matters.</p>";
+    }else if (clues === 3){
+        output.innerHTML = output.innerHTML + "<p>The first 4 numbers.</p>";                
+    }else{
+        clues = 3;
+    }
+    
+})
